0083e348672c3923902bbb6adfe08adf40dd9100
Gabor Nyakas <gabbyka@Gabors-MacBook-Air.local>
2019-01-19 16:36:12 +0100

    added semantic ui

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TopMenu from "./components/topMenu";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <TopMenu />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit!!! <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
---
+++ src/components/topMenu.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+
+const TopMenu = () => {
+  return (
+    <Menu inverted>
+      <Menu.Item header>Navbar</Menu.Item>
+      <Menu.Item header>Blaaa</Menu.Item>
+    </Menu>
+  );
+};
+
+export default TopMenu;
